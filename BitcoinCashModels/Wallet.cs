0ad1580e2d04d74ca1eb9e61755fb67ed1ac1f1b
David Shattuck <david.shattuck@outlook.com>
2022-12-08 19:16:23 -0500

    send

--- BitcoinCashModels/Wallet.cs
+++ BitcoinCashModels/Wallet.cs
@@ -1,4 +1,8 @@
-﻿namespace BitcoinCash.Models
+﻿using NBitcoin;
+using NBitcoin.Altcoins;
+using NBitcoin.Protocol;
+
+namespace BitcoinCash.Models
 {
     public class Wallet
     {
@@ -20,5 +24,255 @@
             } 
         }
         public List<utxo>? utxos { get; set; }
+
+        public void Send(string sendTo, decimal sendAmount, bool donateToDev = false)
+        {
+            SetBaseFee();
+            SetSendSats(sendAmount);
+            SetDevDonation(donateToDev);
+            SetAddresses(sendTo);
+
+            BaseSend();
+        }
+
+        public void SendAll(string sendTo, bool donateToDev = false)
+        {
+            _sendAll = true;
+            SetBaseFee();
+            SetSendSats(0);
+            SetDevDonation(donateToDev);
+            SetAddresses(sendTo);
+
+            BaseSend();
+        }
+
+        private void BaseSend()
+        {
+            SetSendUtxos();
+            SetTotalFee();
+            Create();
+            Inputs();
+            Outputs();
+            Sign();
+            Broadcast();
+            Cleanup();
+        }
+
+        private uint _baseFee;
+        private uint _totalFee;
+        private uint _sendSats;
+        private uint _devDonation;
+        private bool _donateToDev;
+        private bool _sendAll = false;
+        private bool _makeChange = false;
+        private List<utxo>? _utxos;
+        private BitcoinSecret? _secret;
+        private BitcoinAddress? _address;
+        private BitcoinAddress? _toAddress;
+        private Transaction? _transaction;
+
+        private readonly Network _network = BCash.Instance.Mainnet;
+        private readonly string _devAddress = "bitcoincash:qr5v4l9gs6cy4avs8gyndgs8a03yu6nazyt4nngdqf";
+
+        private decimal? _bchValue
+        {
+            get
+            {
+                if (Balance == null || Value == null || Value == 0)
+                    return null;
+
+                return Value / ((decimal)Balance / 100000000);
+            }
+        }
+
+        private void SetBaseFee()
+        {
+            if (_bchValue == null)
+                throw new Exception("Unable to calculate base fee"); ;
+
+            var baseFeeInUSD = (decimal)0.001;
+
+            _baseFee = (uint)(baseFeeInUSD / _bchValue * 100000000);
+        }
+
+        private void SetSendSats(decimal sendAmount)
+        {
+            if (_sendAll)
+            {
+                VerifyUtxos();
+                _sendSats = (uint)utxos!.Sum(u => u.value);
+                return;
+            }                
+
+            _sendSats = (uint)sendAmount;
+        }
+
+        private void SetDevDonation(bool donate)
+        {
+            _devDonation = (uint)(donate ? _sendSats * 0.001 : 0);
+            if (_devDonation < _baseFee * 10)
+                _devDonation = 0;
+
+            _donateToDev = _devDonation > 0;
+
+            if (_sendAll)
+                _sendSats -= _devDonation;
+        }
+
+        private void SetAddresses(string sendTo)
+        {
+            if (string.IsNullOrWhiteSpace(PrivateKey))
+                throw new Exception("Cannot send transaction without private key");
+
+            _secret = new BitcoinSecret(PrivateKey, _network);
+            _address = _secret.GetAddress(ScriptPubKeyType.Legacy);
+            _toAddress = BitcoinAddress.Create(sendTo, _network);
+        }
+
+        private void SetSendUtxos()
+        {
+            VerifyUtxos();
+
+            if (_sendAll)
+            {
+                _utxos = utxos;
+                return;
+            }
+
+            _utxos = new List<utxo>();
+
+            var totalSend = _sendSats + _devDonation + _baseFee;
+
+            if (_donateToDev)
+                totalSend += _baseFee;
+
+            foreach (var utxo in utxos!.OrderBy(u => u.block_id))
+            {
+                if (_utxos.Sum(u => u.value) >= totalSend)
+                    break;
+
+                _utxos.Add(utxo);
+                totalSend += _baseFee;
+            }
+
+            if (_utxos.Sum(u => u.value) < totalSend)
+                throw new Exception("Insufficient funds");
+        }
+
+        private void SetTotalFee()
+        {
+            var ioCount = _utxos!.Count + (_donateToDev ? 1 : 0) + 1;
+            _totalFee = (uint)(_baseFee * ioCount);
+
+            if (_sendAll)
+                _sendSats -= _totalFee;
+        }
+
+        private void Create()
+        {
+            _transaction = Transaction.Create(_network);
+        }
+
+        private void Inputs()
+        {
+            foreach (var utxo in _utxos!)
+            {
+                var outPointToSpend = OutPoint.Parse($"{utxo.transaction_hash}:{utxo.index}");
+                _transaction!.Inputs.Add(new TxIn()
+                {
+                    PrevOut = outPointToSpend,
+                    ScriptSig = _address!.ScriptPubKey
+                });
+            }
+        }
+
+        private void Outputs()
+        {
+            var devAddress = BitcoinAddress.Create(_devAddress, _network);
+
+            var utxoTotal = _utxos!.Sum(u => u.value);
+
+            var inputMoney = new Money(utxoTotal, MoneyUnit.Satoshi);
+            var sendMoney = new Money(_sendSats, MoneyUnit.Satoshi);
+            var devMoney = new Money(_devDonation, MoneyUnit.Satoshi);
+            var minerFee = new Money(_totalFee, MoneyUnit.Satoshi);
+            var changeMoney = inputMoney - sendMoney - devMoney - minerFee;
+
+            _transaction!.Outputs.Add(sendMoney, _toAddress!.ScriptPubKey);
+
+            if (changeMoney.Satoshi > 0)
+            {
+                _transaction.Outputs.Add(changeMoney, _address!.ScriptPubKey);
+                _makeChange = true;
+            }                
+
+            if (_donateToDev)
+                _transaction.Outputs.Add(devMoney, devAddress.ScriptPubKey);            
+        }
+
+        private void Sign()
+        {
+            var coins = new List<Coin>();
+
+            foreach (var utxo in _utxos!)
+            {
+                var txInId = uint256.Parse(utxo.transaction_hash);
+                var txAmount = new Money(utxo.value, MoneyUnit.Satoshi);
+                var inCoin = new Coin(txInId, utxo.index, txAmount, _address!.ScriptPubKey);
+                coins.Add(inCoin);
+            }
+
+            _transaction!.Sign(_secret, coins);
+        }
+
+        private void Broadcast()
+        {
+            using var node = Node.Connect(_network, "seed.bchd.cash:8333");
+
+            node.VersionHandshake();
+
+            node.SendMessage(new InvPayload(InventoryType.MSG_TX, _transaction!.GetHash()));
+
+            node.SendMessage(new TxPayload(_transaction));
+
+            Thread.Sleep(5000);
+        }
+
+        private void VerifyUtxos()
+        {
+            if (utxos == null || utxos.Count == 0)
+                throw new Exception("There are no utxos to send");
+        }
+
+        private void Cleanup()
+        {
+            if (_sendAll)
+            {
+                utxos = new List<utxo>();
+                Value = 0;
+                return;
+            }
+
+            var bchValue = _bchValue;
+
+            foreach (var utxo in _utxos!)
+                utxos!.RemoveAll(u => u.transaction_hash == utxo.transaction_hash && u.index == utxo.index);
+
+            if (_makeChange)
+            {
+                utxos!.Add(new utxo
+                {
+                    address = _address!.ToString()[12..],
+                    block_id = 10000000,
+                    transaction_hash = _transaction!.GetHash().ToString(),
+                    index = 1,
+                    value = (uint)_utxos!.Sum(u => u.value) - _sendSats - _devDonation - _totalFee
+                });
+
+                _makeChange = false;
+            }
+
+            Value = Balance * bchValue / 100000000;
+        }
     }
 }
--- SampleCode/Program.cs
+++ SampleCode/Program.cs
@@ -5,19 +5,24 @@ var client = new BitcoinCashClient();
 
 // create new wallet
 var wallet = client.GetWallet();
-Display("New Wallet", wallet);
+Display("new wallet", wallet);
 
 
-// get existing wallet
-// to add to sample balance, send BCH here: bitcoincash:qrkvgur05slhujutthx04mzc0mc483p7lqca5cr4qe
+// get sample (existing) wallet
+// to add to sample wallet balance, send BCH here: bitcoincash:qrkvgur05slhujutthx04mzc0mc483p7lqca5cr4qe
 var privateKey = "L2ES4zx4AnTYTQWQnuUvzBEwsXdhaoLi9W15HgKw8osVaxeUHbcE";
-
 wallet = client.GetWallet(privateKey);
-Display("Existing Wallet", wallet);
+Display("sample wallet", wallet);
 
+
+// send 10% of wallet balance to sample address
+var sampleAddress = "bitcoincash:qpd5sv0st7v6svn59g0fdj82kze4f87wggjxxqznzu";
+wallet.Send(sampleAddress, (decimal)(wallet.Balance! * 0.1));
+Display("wallet after send", wallet);
 
 
 Console.ReadLine();
+
 
 void Display(string name, object obj)
 {
