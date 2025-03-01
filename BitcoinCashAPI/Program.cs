2afae1cacf84a8e40c150a3e442dd8bb5e2fa0b2
David Shattuck <david.shattuck@outlook.com>
2025-03-01 10:10:47 -0500

    make swagger UI public

--- BitcoinCashAPI/Program.cs
+++ BitcoinCashAPI/Program.cs
@@ -50,11 +50,8 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
